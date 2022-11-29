cf88f764ddd72d0f62ca8ef7ab9924098d652dfe
Sofiyatu Tajudeen <114633109+TowaLites@users.noreply.github.com>
2022-11-29 02:30:16 -0600

    Add files via upload

---
+++ build_docker_image.sh
@@ -0,0 +1,33 @@
+# create a repository to store the docker image in docker hub
+
+# launch an ec2 instance. open port 80 and port 22
+
+# install and configure docker on the ec2 instance
+sudo su -
+yum update -y
+yum install docker -y
+systemctl start docker.service
+systemctl enable docker.service
+usermod -a -G docker ec2-user
+docker info
+
+# create a dockerfile
+ vi Dockerfile 
+
+# build the docker image
+docker build -t techmax .
+
+# login to your docker hub account
+docker login --username tsofiyat
+
+# use the docker tag command to give the image a new name
+docker tag techmax tsofiyat/techmax
+
+# push the image to your docker hub repository
+docker push tsofiyat/techmax
+
+# start the container to test the image 
+docker run -d --name techmax-container -p 80:80 static-app:techmax
+
+# run this command to verify
+docker run -d --name techmax-container -p 80:80 static-app:techmax
